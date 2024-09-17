de313aa2023204ca2aaf9a528cbff10366b0cb38
wedexyz <38834987+wedexyz@users.noreply.github.com>
2024-09-17 18:06:10 +0700

    Add files via upload

---
+++ firebaseConfig.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCRT86JnmAorJyRZQSVBsBUqT1fKeNgmlw",
+    authDomain: "pertamina-dfac1.firebaseapp.com",
+    databaseURL: "https://pertamina-dfac1-default-rtdb.firebaseio.com",
+    projectId: "pertamina-dfac1",
+    storageBucket: "pertamina-dfac1.appspot.com",
+    messagingSenderId: "545759762730",
+    appId: "1:545759762730:web:43ee1c8b97acf5d6b36653",
+    measurementId: "G-JRTF23K4DR"
+  };
+  
+  const app = firebase.initializeApp(firebaseConfig);
---
+++ script2.js
@@ -0,0 +1,272 @@
+
+
+
+data_pompa1     =  firebase.database().ref('pompa1');
+data_pompa2     =  firebase.database().ref('pompa2');
+data_sensorph   =  firebase.database().ref('ph');
+data_sensordht  =  firebase.database().ref('dht');
+data_sensortds  =  firebase.database().ref('tds');
+
+
+
+var slider      = document.getElementById("myRange");
+var output      = document.getElementById("demo");
+var slider2     = document.getElementById("myRange2");
+var output2     = document.getElementById("demo2");
+output.innerHTML    = slider.value;
+output2.innerHTML   = slider2.value;
+
+slider.oninput = function() {
+  output.innerHTML = this.value;
+  firebase.database().ref().update({
+    'pompa1' : this.value
+});
+}
+
+slider2.oninput = function() {
+  output2.innerHTML = this.value;
+  firebase.database().ref().update({
+    'pompa2' : this.value
+});
+}
+
+
+data_pompa1.on("value", function(snapshot) { document.getElementById('data_pompa1').innerHTML=snapshot.val();});
+data_pompa2.on("value", function(snapshot) { document.getElementById('data_pompa2').innerHTML=snapshot.val();});
+
+data_sensorph.on("value", function(snapshot) { document.getElementById('data_sensorph').innerHTML=snapshot.val();});
+data_sensordht.on("value", function(snapshot) { document.getElementById('data_sensordht').innerHTML=snapshot.val();});
+data_sensortds.on("value", function(snapshot) { document.getElementById('data_sensortds').innerHTML=snapshot.val();});
+
+
+Highcharts.stockChart('container1', {
+    chart: {
+        //backgroundColor: 'transparent',
+       // type: 'line',
+        // type: 'areaspline',
+        
+         type: 'spline',
+        events: {
+            load: function () {
+
+                // set up the updating of the chart each second
+                const series = this.series[0];
+                setInterval(function () {
+                        data_sensorph.on("value", function(snapshot) {
+                        const x = (new Date()).getTime(), // current time
+                        y= snapshot.val()
+                        series.addPoint([x, y], true, true);
+                });
+                }, 1000);
+            }
+        }
+    },
+
+    accessibility: {
+        enabled: false
+    },
+
+    time: {
+        useUTC: false
+    },
+
+    rangeSelector: {
+        buttons: [{
+            count: 1,
+            type: 'minute',
+            text: '1M'
+        }, {
+            count: 5,
+            type: 'minute',
+            text: '5M'
+        }, {
+            type: 'all',
+            text: 'All'
+        }],
+        inputEnabled: false,
+        selected: 0
+    },
+
+    title: {
+        text: 'Sensor PH'
+    },
+
+    exporting: {
+        enabled: true
+    },
+
+    series: [{
+        name: 'PH',
+        data: (function () {
+            // generate an array of random data
+            const data = [],
+                time = (new Date()).getTime();
+
+            for (let i = -999; i <= 0; i += 1) {
+                data.push([
+                    time + i * 1000,0
+                   // Math.round(Math.random() * 100)
+                
+                ]);
+            }
+            return data;
+        }())
+    }]
+});
+
+
+Highcharts.stockChart('container2', {
+    chart: {
+        events: {
+            load: function () {
+
+                // set up the updating of the chart each second
+                const series = this.series[0];
+                setInterval(function () {
+                    data_sensordht.on("value", function(snapshot) {
+                    const x = (new Date()).getTime(), // current time
+                    y= snapshot.val()
+                series.addPoint([x, y], true, true);
+            });
+            },1000);
+            }
+        }
+    },
+
+    accessibility: {
+        enabled: false
+    },
+
+    time: {
+        useUTC: false
+    },
+
+    rangeSelector: {
+        buttons: [{
+            count: 1,
+            type: 'minute',
+            text: '1M'
+        }, {
+            count: 5,
+            type: 'minute',
+            text: '5M'
+        }, {
+            type: 'all',
+            text: 'All'
+        }],
+        inputEnabled: false,
+        selected: 0
+    },
+
+    title: {
+        text: 'Sensor DHT'
+    },
+
+    exporting: {
+        enabled: true
+    },
+   
+    series: [{
+        name: 'DHT',
+        data: (function () {
+            // generate an array of random data
+            const data = [],
+                time = (new Date()).getTime();
+            
+            for (let i = -999; i <= 0; i += 1) {
+               
+                
+               
+                data.push([
+                    time + i * 1000,
+                    0
+                   // Math.round(Math.random() * 100)
+                  
+                ]);
+           
+            }
+            return data;
+        }
+        
+        
+        ())
+    }]
+});
+
+Highcharts.stockChart('container3', {
+    chart: {
+        events: {
+            load: function () {
+
+                // set up the updating of the chart each second
+                const series = this.series[0];
+                setInterval(function () {
+                    data_sensortds.on("value", function(snapshot) {
+                    const x = (new Date()).getTime(), // current time
+                    y= snapshot.val()
+                series.addPoint([x, y], true, true);
+            });
+            },1000);
+            }
+        }
+    },
+
+    accessibility: {
+        enabled: false
+    },
+
+    time: {
+        useUTC: false
+    },
+
+    rangeSelector: {
+        buttons: [{
+            count: 1,
+            type: 'minute',
+            text: '1M'
+        }, {
+            count: 5,
+            type: 'minute',
+            text: '5M'
+        }, {
+            type: 'all',
+            text: 'All'
+        }],
+        inputEnabled: false,
+        selected: 0
+    },
+
+    title: {
+        text: 'Sensor TDS'
+    },
+
+    exporting: {
+        enabled: true
+    },
+   
+    series: [{
+        name: 'TDS',
+        data: (function () {
+            // generate an array of random data
+            const data = [],
+                time = (new Date()).getTime();
+            
+            for (let i = -999; i <= 0; i += 1) {
+               
+                
+               
+                data.push([
+                    time + i * 1000,
+                    0
+                   // Math.round(Math.random() * 100)
+                  
+                ]);
+           
+            }
+            return data;
+        }
+        
+        
+        ())
+    }]
+});
